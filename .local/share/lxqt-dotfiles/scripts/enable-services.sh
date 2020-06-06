49182ab2a4909bbb87abc8fdd7a3271acb7c3fd8
User <david.murko@mailbox.org>
2020-06-06 15:54:22 +0200

    More scripts + taskbar active windows

---
+++ .local/share/lxqt-dotfiles/scripts/enable-services.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+ln -s /etc/sv/NetworkManager /var/service
+ln -s /etc/sv/lightdm /var/service
---
+++ .local/share/lxqt-dotfiles/scripts/install-packages.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xbps-install -S xorg lxqt pulseaudio NetworkManager nm-tray firefox lightdm-gtk3-greeter
