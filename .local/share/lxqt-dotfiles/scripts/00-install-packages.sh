84c5b007e326a218acb56fef5fcc2ab7000aa442
root <david.murko@mailbox.org>
2020-06-06 16:37:22 +0200

    pcmanfm settings + install gvfs

--- .local/share/lxqt-dotfiles/scripts/00-install-packages.sh
+++ .local/share/lxqt-dotfiles/scripts/00-install-packages.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xbps-install -S xorg lxqt pulseaudio pavucontrol-qt NetworkManager nm-tray dbus lightdm-gtk3-greeter chrony
+xbps-install -S xorg lxqt pulseaudio pavucontrol-qt NetworkManager nm-tray dbus lightdm-gtk3-greeter chrony gvfs
