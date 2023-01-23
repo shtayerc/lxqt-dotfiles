43196d1061cee7599df4172a7184f9f67525a5c1
shtayerc <david.murko@mailbox.org>
2023-01-24 00:33:23 +0100

    Revert "for some reason multiline -p does not work"
    This reverts commit 2b8f38823b12cce3878cc13ab0beb8d3d5cb5a8a.

--- lxqt-dot.sh
+++ lxqt-dot.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
 ARCH=x86_64
-REPO=https://repo-de.voidlinux.org/current
+REPO=https://alpha.de.repo.voidlinux.org/current
 DIR="$(basename $PWD)"
-UBLOCK_URL='https://addons.mozilla.org/firefox/downloads/file/4047353/ublock_origin-1.46.0.xpi'
+UBLOCK_URL='https://addons.mozilla.org/firefox/downloads/file/4003969/ublock_origin-1.44.4.xpi'
 SERVICES="agetty-tty1 agetty-tty2 agetty-tty3 uuidd udevd dbus polkitd rtkit ntpd lightdm NetworkManager cupsd saned"
-PACKAGES="$(grep '^[^#].' ${DIR}/lxqt-dot.packages | tr '\n' ' ')"
 cd ..
 curl $UBLOCK_URL -o ${DIR}'/includedir/usr/lib/firefox/browser/extensions/uBlock0@raymondhill.net.xpi'
-./mklive.sh -a ${ARCH} -r "${REPO}/nonfree" -p $PACKAGES -I "${DIR}/includedir" -T "Void Linux LXQt" -o "${DIR}/lxqt-dotfiles-${ARCH}.iso" -S "${SERVICES}"
+./mklive.sh $@ \
+    -a ${ARCH} \
+    -r "${REPO}/nonfree" \
+    -p "$(grep '^[^#].' ${DIR}/lxqt-dot.packages)" \
+    -T "Void Linux LXQt" \
+    -I "${DIR}/includedir" \
+    -o "${DIR}/lxqt-dotfiles-${ARCH}.iso" \
+    -S "${SERVICES}"
 cd -
