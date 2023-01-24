cb21abf5ce14ff7f84bc4acaf2f5d7370a2941e8
shtayerc <david.murko@mailbox.org>
2023-01-24 21:06:31 +0100

    ublock: update to 1.46.0

--- lxqt-dot.sh
+++ lxqt-dot.sh
@@ -3,7 +3,7 @@
 ARCH=x86_64
 REPO=https://alpha.de.repo.voidlinux.org/current
 DIR="$(basename $PWD)"
-UBLOCK_URL='https://addons.mozilla.org/firefox/downloads/file/4003969/ublock_origin-1.44.4.xpi'
+UBLOCK_URL='https://addons.mozilla.org/firefox/downloads/file/4047353/ublock_origin-1.46.0.xpi'
 SERVICES="agetty-tty1 agetty-tty2 agetty-tty3 uuidd udevd dbus polkitd rtkit ntpd lightdm NetworkManager cupsd saned"
 cd ..
 curl $UBLOCK_URL -o ${DIR}'/includedir/usr/lib/firefox/browser/extensions/uBlock0@raymondhill.net.xpi'
